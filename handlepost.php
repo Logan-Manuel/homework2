4c2a4c00b5b61a0d8f12aae4ce3f9462714532b8
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-09-26 13:46:59 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,13 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  </head>
-  
 
+  
+<?php require_once("header.php"); ?>
+<h1> My website </h1>
+  
   <body>
     <h1>Get Response</h1>
     Welcome <?php echo $_GET["name"]; ?><br>
